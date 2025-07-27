fec20f4fed36e01ab5312ac8fc7e904dfce01c96
Tavisha10 <Tavishapearl10@gmail.com>
2025-07-27 23:31:48 +0530

    Add deployment dependencies

--- payrollsystem/payrollsystem/settings.py
+++ payrollsystem/payrollsystem/settings.py
@@ -75,16 +75,19 @@ WSGI_APPLICATION = 'payrollsystem.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'payroll',
-        'USER': 'root',
-        'PASSWORD': 'shelter1',  # your actual MySQL password
-        'HOST': 'localhost',
-        'PORT': '3306',
+'default': {
+    'ENGINE': 'django.db.backends.mysql',
+    'NAME': 'Tavisha10$payroll',      # Use your actual username and database name
+    'USER': 'Tavisha10',              # Your PythonAnywhere username
+    'PASSWORD': 'shelter1',  # Set during DB creation
+    'HOST': 'Tavisha10.mysql.pythonanywhere-services.com',  # Standard PA hostname
+    'PORT': '3306',
     }
 }
+
+
 
 
 # Password validation
